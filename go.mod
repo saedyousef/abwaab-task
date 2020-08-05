c3fb9297826d892869d3353204daec75a5a51c29
Saed Yousef <saed.alzaben@gmail.com>
2020-08-05 22:37:04 +0300

    adding .env file

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/jinzhu/gorm v1.9.15
+	github.com/joho/godotenv v1.3.0
 	github.com/twinj/uuid v1.0.0
 	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
 )
--- tweeters/twitter.go
+++ tweeters/twitter.go
@@ -1,15 +1,28 @@
 package tweeters
 
 import (
+	"os"
+	"github.com/joho/godotenv"
 	"github.com/dghubble/oauth1"
 	"github.com/dghubble/go-twitter/twitter"
 )
-
+type error interface {
+    Error() string
+}
 
 func SearchTweets(query string) []string {
 
-	config := oauth1.NewConfig("r5OdTex1cn0cez6x6dJWNAMgS", "Ib9q2rj7CTPDLm2VPYPE27bYrciSoYoBnJmg9qv08geocffNDz")
-	token := oauth1.NewToken("1322735486-IwPcAxffIBhWeHZT1XldW3VqD4LNdGrUqMF3ewA", "Quhyx9rtjiRB45NoGQmyWK8L6zq4Jxb3zHKxDHSMX5SWZ")
+	var e []string
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		er := append(e, err.Error())
+		return er
+	}
+
+	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
+	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("TOKEN_SECRET"))
 	httpClient := config.Client(oauth1.NoContext, token)
 
 	// Twitter client
@@ -24,7 +37,7 @@ func SearchTweets(query string) []string {
 
 	// Tweets Array.
 	var tweets []string
-	
+
 	// Filling the tweets array.
 	for _, tweet := range search.Statuses {
 		tweets = append(tweets, tweet.Text, tweet.CreatedAt)
