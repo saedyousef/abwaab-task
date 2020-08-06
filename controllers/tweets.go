e2e2bab499e2d98fa47c86e3cc017505e3d76737
Saed Yousef <saed.alzaben@gmail.com>
2020-08-06 17:54:54 +0300

    create tweet handled

--- controllers/tweets.go
+++ controllers/tweets.go
@@ -3,29 +3,15 @@ package controllers
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/saedyousef/abwaab-task/auth"
 	"github.com/saedyousef/abwaab-task/models"
 	"github.com/saedyousef/abwaab-task/tweeters"
 )
 
 type CreateTweetInput struct {
 	Body  string `json:"body" binding:"required"`
+	UserID uint64 `json:"user_id"`
 }
-
-func CreateTweet(c *gin.Context) {
-	// Validate input
-	var input CreateTweetInput
-	if err := c.ShouldBindJSON(&input); err != nil {
-	  	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	  	return
-	}
-  
-	// Create book
-	tweet := models.Tweet{Body: input.Body}
-	models.DB.Create(&tweet)
-  
-	c.JSON(http.StatusOK, gin.H{"data": tweet})
-}
-
 
 // This api call a function that uses Twitter Client to get tweets.
 func SearchTweets(c *gin.Context) {
@@ -40,4 +26,27 @@ func SearchTweets(c *gin.Context) {
 	tweets := tweeters.SearchTweets(url["query"][0])
 	
 	c.JSON(http.StatusOK, tweets)
+}
+
+func CreateTweet(c *gin.Context) {
+	var input CreateTweetInput
+
+  	// Validate input
+	if err := c.ShouldBindJSON(&input); err != nil {
+	  	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	  	return
+	}
+
+	// Extracting the token to get the userid.
+	tokenAuth, err := auth.ExtractTokenMetadata(c.Request)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	
+	tweet := models.Tweet{Body: input.Body, UserID: tokenAuth.UserId}
+	models.DB.Create(&tweet)
+
+	c.JSON(http.StatusCreated, gin.H{"data": tweet})
 }
--- models/tweets.go
+++ models/tweets.go
@@ -5,4 +5,5 @@ package models
 type Tweet struct {
 	ID   uint   `json:"id" gorm:"primary_key"`
 	Body string `json:"body"`
+	UserID uint64 `json:"user_id"`
 }
